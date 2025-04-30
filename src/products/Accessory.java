32a37957df6d2354f47aa9fbeae787700bcaa975
farnaz <whoisfarnaz@gmail.com>
2025-04-30 13:14:27 +0330

    add AccessoryClass

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -1,4 +1,24 @@
 package products;
 
-public class Accessory {
+public class Accessory extends Product {
+    private String color;
+
+    public Accessory(String color, String title, Double price) {
+        super(title, price);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Color: " + color;
+    }
+
+    @Override
+    protected String generateId() {
+        return getNextId();
+    }
 }
