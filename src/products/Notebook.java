df442215521052d392447d0c5996a6e0ed883ef2
farnaz <whoisfarnaz@gmail.com>
2025-04-30 13:13:58 +0330

    add NotebookClass

--- src/products/Notebook.java
+++ src/products/Notebook.java
@@ -1,4 +1,30 @@
 package products;
 
-public class Notebook {
+public class Notebook extends Product {
+    private int pageCount;
+    private boolean isHardCover;
+
+    public Notebook(int pageCount, boolean isHardCover, String title, Double price) {
+        super(title, price);
+        this.pageCount = pageCount;
+        this.isHardCover = isHardCover;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public boolean isHardCover() {
+        return isHardCover;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " PageCount: " + pageCount + " HardCover: " + isHardCover;
+    }
+
+    @Override
+    protected String generateId() {
+        return getNextId();
+    }
 }
