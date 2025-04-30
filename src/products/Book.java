e01ae36979b802f8c988a1f766c1b55a24e13786
farnaz <whoisfarnaz@gmail.com>
2025-04-30 13:13:18 +0330

    add BookClass

--- src/products/Book.java
+++ src/products/Book.java
@@ -1,4 +1,36 @@
 package products;
 
-public class Book {
+public class Book extends Product {
+    private String author;
+    private String publication;
+    private String genre;
+
+    public Book(String author, String publication, String genre, String title, Double price) {
+        super(title, price);
+        this.author = author;
+        this.publication = publication;
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublication() {
+        return publication;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Author: " + author + " Publication: " + publication + " Genre: " + genre;
+    }
+
+    @Override
+    protected String generateId() {
+        return getNextId();
+    }
 }
